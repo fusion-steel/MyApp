a1d975017af6eeeec78f7e01454fcac50a5ab3a2
Fusion <lethal@mail.ua>
2021-01-30 21:56:56 +0200

    Add

--- src/main/kotlin/example/myapp/AquariumFish.kt
+++ src/main/kotlin/example/myapp/AquariumFish.kt
@@ -1,19 +1,36 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
+class Shark(fishColor: FishColor = GrayColor):
+    FishAction by PrintingFishAction("hunt and eat fish"),
+    FishColor by fishColor {
 }
 
-class Shark: AquariumFish(), FishAction {
-    override val color: String = "gray"
+class Plecostomus(fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor {
+}
+
+object GoldColor: FishColor {
+    override val color = "gold"
+}
+
+object GrayColor: FishColor {
+    override val color = "gray"
+}
+
+class PrintingFishAction(val food: String): FishAction {
     override fun eat() {
-        println("yum")
+        println(food)
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
-    override val color = "gold"
-    override fun eat() {
-        println("eat algae")
+sealed class Seal
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal): String {
+    return when(seal) {
+        is Walrus -> "walrus"
+        is SeaLion -> "sea lion"
     }
 }
--- src/main/kotlin/example/myapp/FishAction.kt
+++ src/main/kotlin/example/myapp/FishAction.kt
@@ -2,10 +2,4 @@ package example.myapp
 
 interface FishAction {
     fun eat()
-//    fun jump()
-//    fun clean()
-//    fun catchFish()
-//    fun swim() {
-//        println("swim")
-//    }
 }
